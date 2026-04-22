457e2330ba05233c282da04a5c2ef6d780c9bbad
adamboyer <alexander.boyer75@gmail.com>
2026-04-22 00:07:14 -0500

    Fix java.lang.NumberFormatException: incident f6e27cd2

--- src/main/java/com/demo/service/UserService.java
+++ src/main/java/com/demo/service/UserService.java
@@ -6,10 +6,6 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     public String getUserName(String id) {
-        // INTENTIONAL BUG:
-        // Will throw NumberFormatException if id is not numeric
-        int userId = Integer.parseInt(id);
-
-        return "User-" + userId;
+        return "User-" + id;
     }
 }
